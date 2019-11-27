332a71200777ad03210c5cc5a5d1e47eedf477b0
Dineshraina108 <esecdinesh@gmail.com>
2019-11-27 18:00:31 +0530

    My project updated

---
+++ README.md
@@ -0,0 +1,2 @@
+# myproject
+my practice programs 
---
+++ samplemvc/samplemvc/Controllers/firstController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace samplemvc.Controllers
+{
+    public class firstController : Controller
+    {
+        //
+        // GET: /first/
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+	}
+}
